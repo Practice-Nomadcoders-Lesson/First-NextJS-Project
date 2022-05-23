5de3cbe97070d21aac1c6d50e0fe92c7520755b9
iinsue <hinsu93@naver.com>
2022-05-23 11:31:52 +0900

    #2.6 Movie Detail

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
           <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+              key={movie.id}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>{`
@@ -32,6 +63,9 @@ export default function Home({ results }) {
         .movie h4 {
           font-size: 18px;
           text-align: center;
+        }
+        a {
+          text-decoration: none;
         }
       `}</style>
     </div>
